ec383ad763ee41af5f2d800f9e9f12e3ad7f9350
Stian Tofte <stitofte@outlook.com>
2018-12-18 14:52:49 +0100

    re writing the deathListener.

--- DeathVault/src/com/onestian/deathvault/deathListener.java
+++ DeathVault/src/com/onestian/deathvault/deathListener.java
@@ -1,18 +1,16 @@
 package com.onestian.deathvault;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.bukkit.Location;
 import org.bukkit.Material;
-import org.bukkit.World;
-import org.bukkit.block.Block;
 import org.bukkit.block.Chest;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDeathEvent;
-import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 
 public class deathListener implements Listener {
@@ -20,39 +18,40 @@ public class deathListener implements Listener {
 	@EventHandler
 	public void onDeath(EntityDeathEvent event) {
 		
-		String ply = null;
-		World playerWorld = null;
-		Location deathLoc = null;
-		Block locBlock = null;
-		
 		if (event.getEntityType() == EntityType.PLAYER) {
-			ply = event.getEntity().getName();
-			Player pl = deathvault.thisPlugin.getServer().getPlayer(ply);
-			playerWorld = pl.getWorld();
-			deathLoc = event.getEntity().getLocation();
 			
-			List<ItemStack> iStack = event.getDrops();
-			ItemStack[] i2ForChest = iStack.toArray(new ItemStack[0]);			
+			Player player = deathvault.thisPlugin.getServer().getPlayer(event.getEntity().getName());
+			Location loc = player.getLocation();
 			
-			event.getDrops().removeAll(iStack);
+			final List<ItemStack> content = new ArrayList<ItemStack>();
+			final ItemStack[] items = content.toArray(new ItemStack[content.size()]);
 			
-			int x = deathLoc.getBlockX();
-			int y = deathLoc.getBlockY();
-			int z = deathLoc.getBlockZ();
+			//Grabbing items from dead player!
+			for (final ItemStack item : event.getDrops()) {
+				content.add(item);
+				deathvault.thisPlugin.getLogger().info(item.toString());
+			}
 			
+			//Placing chests on death location
+			final double x = loc.getX();
 			
-			Location deathChest = new Location(playerWorld, x, y, z);
-			locBlock = deathChest.getBlock();
+			final Location x1 = loc.clone();
+			final Location x2 = loc.clone();
 			
-			locBlock.setType(Material.CHEST);
+			x2.setX(x + 1);
 			
-			Chest chest = (Chest) locBlock.getState();
-			Inventory cInv = chest.getInventory();
-			cInv.setContents(i2ForChest);
+			for (final Location cLoc : new Location[] {x1,x2}) {
+				cLoc.getBlock().setType(Material.CHEST);
+			}
 			
+			Chest chest = (Chest) x1.getBlock().getState();
+			chest.setCustomName(player.getName() + "'s death chest!");
+			//chest.setLock("123");
+			chest.update(true, true);
 			
-			messageSender.messagePlayer("Chest has been spawned with your items, where you died.", ply);
-			messageSender.messagePlayer("You died.. Get to your chest! Quick! Before someone else gets it!", ply);
+			deathvault.thisPlugin.getLogger().info(player.getLocation().getBlock().toString());
+			
+			messageSender.messagePlayer("You died.. Get to your chest! Quick! Before someone else gets it!", player);
 		}
 	}
 }
